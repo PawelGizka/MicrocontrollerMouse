7ca01f9fc1ba1e9f84e162cbc433be1e0821317a
Paweł Giżka <pawelgizka@gmail.com>
2018-12-16 11:09:08 +0100

    interrupts working.

--- leds_main.c
+++ leds_main.c
@@ -27,7 +27,19 @@ int charsToSend = 0;
 
 char tempBuffer[SEND_BUFFER_SIZE];
 
+volatile int gyroReady = 0;
+
+int wasDataSend = 0;
+int wasRepeatedStart = 0;
+
 volatile irq_level_t currentLevel;
+
+void intToChar(char* buffer, int value, int offset) {
+    for (int j = offset; value != 0 && j > 0; j--) {
+        buffer[j] = (value % 10) + '0';
+        value /= 10;
+    }
+}
 
 void sendToBuffer(const char* command) {
     currentLevel = IRQprotectAll();
@@ -158,12 +170,19 @@ void configureGyro() {
 
     I2C1->CR1 |= I2C_CR1_PE;
 
+    //przerwania
+    I2C1->CR2 |= I2C_CR2_ITEVTEN | I2C_CR2_ITERREN;
+
+    NVIC_EnableIRQ(I2C1_EV_IRQn);
+    NVIC_EnableIRQ(I2C1_ER_IRQn);
+
 }
 
 void saveValueToRegister() {
     //Zainicjuj transmisję sygnału START
     I2C1->CR1 |= I2C_CR1_START;
 
+    /*
     //Czekaj na zakończenie transmisji bitu START, co jest
     //sygnalizowane ustawieniem bitu SB (ang.start bit) w rejestrze SR1,
     //czyli czekaj na spełnienie warunku
@@ -231,12 +250,14 @@ void saveValueToRegister() {
 
     //Zainicjuj transmisję sygnału STOP
     I2C1->CR1 |= I2C_CR1_STOP;
+     */
 }
 
-int readFromGyro() {
+void readFromGyro() {
     //Zainicjuj transmisję sygnału START
     I2C1->CR1 |= I2C_CR1_START;
 
+    /*
     //Czekaj na ustawienie bituSBw rejestrze SR1, warunek
     int i = 0;
     while (!(I2C1->SR1 & I2C_SR1_SB) && i <= MAX_WAIT_VALUE) {
@@ -335,6 +356,223 @@ int readFromGyro() {
     int value = I2C1->DR;
 
     return value;
+    */
+}
+
+int firstBtf = 1;
+
+
+void I2C1_EV_IRQHandler(void) {
+    if (gyroReady) {
+        if (I2C1->SR1 & I2C_SR1_SB) {
+            if (!wasRepeatedStart) {
+                //1
+                sendToBuffer("I2C_SR1_SB 1\n\r");
+                sendBufferToDMA();
+
+                //Zainicjuj wysyłanie 7-bitowego adresu slave’a, tryb MT
+                I2C1->DR = LIS35DE_ADDR << 1;
+            }  else {
+                //4
+                sendToBuffer("I2C_SR1_SB 4\n\r");
+                sendBufferToDMA();
+
+                //Zainicjuj wysyłanie 7-bitowego adresu slave’a, tryb MR
+                I2C1->DR = (LIS35DE_ADDR << 1) | 1U;
+
+
+                //Ustaw, czy po odebraniu pierwszego bajtu ma być wysłany sygnał ACK czy NACK
+                //Ponieważ ma być odebrany tylko jeden bajt, ustaw wysłanie sygnału NACK, zerując bit ACK
+                I2C1->CR1 &= ~I2C_CR1_ACK;
+            }
+
+        }
+
+        if (I2C1->SR1 & I2C_SR1_ADDR) {
+            if (!wasRepeatedStart) {
+                //2
+                sendToBuffer("I2C_SR1_ADDR 2\n\r");
+                sendBufferToDMA();
+
+                //Skasuj bit ADDR przez odczytanie rejestru SR2 po odczytaniu rejestru SR1
+                I2C1->SR2;
+
+                //Zainicjuj wysyłanie numeru rejestru slave’a
+                I2C1->DR = OUT_Y;
+            } else {
+                //5
+                sendToBuffer("I2C_SR1_ADDR 5\n\r");
+                sendBufferToDMA();
+
+                I2C1->CR2 |= I2C_CR2_ITBUFEN;
+
+                //Skasuj bit ADDR
+                I2C1->SR2;
+
+                //Zainicjuj transmisję sygnału STOP, aby został wysłany po
+                //odebraniu ostatniego (w tym przypadku jedynego) bajtu
+                I2C1->CR1 |= I2C_CR1_STOP;
+            }
+        }
+
+
+        if (I2C1->SR1 & I2C_SR1_BTF) {
+            //3
+            sendToBuffer("I2C_SR1_BTF 3\n\r");
+            sendBufferToDMA();
+
+            if (firstBtf) {
+                wasRepeatedStart = 1;
+
+                //Zainicjuj transmisję sygnału REPEATED START
+                I2C1->CR1 |= I2C_CR1_START;
+            } else {
+
+            }
+
+        }
+
+        if (I2C1->SR1 & I2C_SR1_RXNE) {
+            //6
+            sendToBuffer("I2C_SR1_RXNE 6\n\r");
+            sendBufferToDMA();
+
+            //Odczytaj odebraną 8-bitową wartość
+            int value = I2C1->DR;
+
+            if (value != 0) {
+                char num[32];
+                for (int j = 0; j < 32; j++) {
+                    num[j] = 0;
+                }
+
+                intToChar(num, value, 29);
+
+                num[30] = '\n';
+                num[31] = '\r';
+
+                sendToBuffer(num);
+
+                sendToBuffer("dziala 1\n\r");
+            } else {
+                sendToBuffer("dziala 2\n\r");
+            }
+            sendBufferToDMA();
+        }
+
+
+    } else {
+        int samePass = 0;
+        if (I2C1->SR1 & I2C_SR1_SB) {
+
+            sendToBuffer("I2C_SR1_SB\n\r");
+            sendBufferToDMA();
+
+            //Zainicjuj wysyłanie 7-bitowego adresu slave’a, tryb MT
+            I2C1->DR = LIS35DE_ADDR << 1;
+
+        }
+
+        if (I2C1->SR1 & I2C_SR1_ADDR) {
+            sendToBuffer("I2C_SR1_ADDR\n\r");
+            sendBufferToDMA();
+
+            samePass = 1;
+
+            I2C1->CR2 |= I2C_CR2_ITBUFEN;
+            wasDataSend = 1;
+
+            //Skasuj bit ADDR przez odczytanie rejestru SR2 po odczytaniu rejestru SR1
+            I2C1->SR2;
+
+            //Zainicjuj wysyłanie 8-bitowego numeru rejestru slave’a
+            I2C1->DR = CTRL_REG1;
+        }
+
+        if ((I2C1->SR1 & I2C_SR1_TXE) && wasDataSend && !samePass) {
+            sendToBuffer("I2C_SR1_TXE\n\r");
+            sendBufferToDMA();
+
+            I2C1->CR2 &= (~((uint32_t)0x00000000)) ^ I2C_CR2_ITBUFEN;
+            wasDataSend = 0;
+
+            uint32_t powerUp = 1 << 6;
+            uint32_t zEnabled = 1 << 2;
+            uint32_t yEnabled = 1 << 1;
+            uint32_t xEnabled = 1;
+            uint32_t reqValue =  (zEnabled | yEnabled | xEnabled | powerUp);
+
+            //Wstaw do kolejki nadawczej 8-bitową wartość zapisywaną do rejestru slave’a
+            I2C1->DR = reqValue;
+        }
+
+        if (I2C1->SR1 & I2C_SR1_BTF) {
+            sendToBuffer("I2C_SR1_BTF\n\r");
+            sendBufferToDMA();
+
+            if (firstBtf) {
+                firstBtf = 0;
+                I2C1->CR1 |= I2C_CR1_STOP;
+            } else {
+                gyroReady = 1;
+                firstBtf = 1;
+
+                int i = 0;
+                while (i < 10000000) {
+                    __NOP();
+                    i++;
+                }
+
+                readFromGyro();
+            }
+        }
+    }
+
+}
+
+void I2C1_ER_IRQHandler(void) {
+
+    sendToBuffer("Error at I2C occur\n\r");
+    sendBufferToDMA();
+}
+
+void configureCounter() {
+    //Włączenie taktowania układu licznika
+    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+
+    //zliczaj w gore
+    TIM3->CR1 = 0;
+
+    //Skonfigurowanie preskalera i zakresu zliczania
+    TIM3->PSC = 20;
+    TIM3->ARR = 3200000000;//10s
+
+    //wymuszenie zdarzenia uaktualnienia
+    TIM3->EGR = TIM_EGR_UG;
+
+    //Uruchomienie licznika
+    TIM3->CR1 |= TIM_CR1_CEN;
+
+    //wlaczenie przerwania
+    TIM3->SR = ~(TIM_SR_UIF | TIM_SR_CC1IF);
+    TIM3->DIER = TIM_DIER_UIE | TIM_DIER_CC1IE;
+
+    NVIC_EnableIRQ(TIM3_IRQn);
+
+
+}
+
+void TIM3_IRQHandler(void) {
+    uint32_t it_status = TIM3->SR & TIM3->DIER;
+    if (it_status & TIM_SR_UIF) {
+        TIM3->SR = ~TIM_SR_UIF;
+        //uaktualnienie
+    }
+    if (it_status & TIM_SR_CC1IF) {
+        TIM3->SR = ~TIM_SR_CC1IF;
+//        sendToBuffer("licznik\n\r");
+//        sendBufferToDMA();
+    }
 }
 
 
@@ -347,10 +585,12 @@ int main() {
 
     configureUsartAndDma();
 
+    configureGyro();
+
+//    configureCounter();
+
     sendToBuffer("break 1\n\r");
     sendBufferToDMA();
-
-    configureGyro();
 
     sendToBuffer("break 2\n\r");
     sendBufferToDMA();
@@ -364,24 +604,31 @@ int main() {
 
     int i = 0;
 
+
     while (1) {
+        if (!gyroReady) {
+            __NOP();
+            continue;
+        }
+
         i++;
-        if (i > INT32_MAX - 1) {
+        if (i > 10000000) {
+
+//            sendToBuffer("Start reading\n\r");
+//            sendBufferToDMA();
+
+//            readFromGyro();
+            /*
             int value = readFromGyro();
 
+
             if (value != 0) {
-
                 char num[32];
-
                 for (int j = 0; j < 32; j++) {
                     num[j] = 0;
                 }
 
-                int x = value;
-                for (int j = 29; x != 0 && j > 0; j--) {
-                    num[j] = (x % 10) + '0';
-                    x /= 10;
-                }
+                intToChar(num, value, 29);
 
                 num[30] = '\n';
                 num[31] = '\r';
@@ -404,13 +651,14 @@ int main() {
 //                char buf[3] = "3\n\r";
 //
 //                sendToBuffer(num);
-                sendToBuffer("0\n\r");
+                sendToBuffer("nie dziala\n\r");
 //
             }
 
+            */
 
-            sendBufferToDMA();
-            i = 0;
+//            sendBufferToDMA();
+//            i = 0;
         }
 
     }
